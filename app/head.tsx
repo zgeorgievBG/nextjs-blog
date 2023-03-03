4c3f3222b4b4b3a948c1442eaa457d0bbbcae8c7
zgeorgievBG <79528041+zgeorgievBG@users.noreply.github.com>
2023-03-03 21:03:16 +0200

    initial commit

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Head() {
+    return (
+        <>
+        <title>NextJS Blog</title>
+        </>
+    )
+}
+
+export default Head
---
+++ nextjs-blog/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'kit3vz7h',
+    dataset: 'production'
+  }
+})
